d3212352c19d5d3829aae666ff67eab6ce66bb61
sreekanth56 <koye.sree@gmail.com>
2020-03-17 18:28:23 +0530

    Basic Html Setup of TDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-template-driven-forms';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
